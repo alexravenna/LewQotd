3afd0ee898ec0f774e29c4be5ceedb796cd64087
ailci <ali.ilci@gmx.de>
2024-04-16 15:27:30 +0200

    Qotd

---
+++ Qotd.Shared/Model/QuoteOfTheDayViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qotd.Shared.Model;
+
+public class QuoteOfTheDayViewModel
+{
+    public Guid Id { get; set; }
+    public string QuoteText { get; set; } = string.Empty;
+    public string AuthorName { get; set; } = string.Empty;
+    public string AuthorDescription { get; set; } = string.Empty;
+    public DateOnly? AuthorBirthDate { get; set; }
+    public byte[]? AuthorPhoto { get; set; }
+    public string? AuthorPhotoMimeType { get; set; }
+}
