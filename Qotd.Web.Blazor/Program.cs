27ae0dea6483cc5ceb899bc6f9aa15b0ecf24fe6
ailci <ali.ilci@gmx.de>
2024-04-16 14:34:21 +0200

    Add project files.

---
+++ Qotd.Web.Blazor/Program.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Qotd.Web.Blazor.Components;
+using Qotd.Web.Blazor.Components.Account;
+using Qotd.Web.Blazor.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+var app = builder.Build();
+
+//Startbeispiel #############################################
+//app.Use(async (context, next) =>
+//{
+//    //var userAgent = context.Request.Headers["User-Agent"];
+//    await context.Response.WriteAsync("Erste middleware\n");  //Hinweg
+//    await next();
+//    await context.Response.WriteAsync("Erste middleware Back\n"); //Rückweg
+//});
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Zweite middleware\n");
+//    await next();
+//    await context.Response.WriteAsync("Zweite middleware Back\n");
+//});
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Ende middleware\n");
+//});
+
+
+
+// Configure the HTTP request pipeline. ################################################################
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
