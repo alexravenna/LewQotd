a77a8af6b046d8a34405413123a550e614411342
ailci <ali.ilci@gmx.de>
2024-04-18 15:02:16 +0200

    Photo

---
+++ Qotd.Shared/Utilities/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Qotd.Shared.Utilities;
+
+public static class Util
+{
+    public static string LogAsJson<T>(this T obj, bool writeIntended = true) where T : class
+    {
+        return JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = writeIntended });
+    }
+}
--- Qotd.Web.Blazor/Components/Pages/Author/NewAuthor.razor.cs
+++ Qotd.Web.Blazor/Components/Pages/Author/NewAuthor.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Qotd.Shared.Model;
+using Qotd.Shared.Utilities;
 
 namespace Qotd.Web.Blazor.Components.Pages.Author;
 public partial class NewAuthor
@@ -8,4 +10,16 @@ public partial class NewAuthor
     public AuthorForCreateViewModel? AuthorForCreateVm { get; set; }
 
     protected override void OnInitialized() => AuthorForCreateVm ??= new();
+    [Inject] public ILogger<NewAuthor> Logger { get; set; } = default!;
+
+    private Task HandleValidSubmit(EditContext arg)
+    {
+        Logger.LogInformation($"Valid Submit aufgerufen... {AuthorForCreateVm?.LogAsJson()}");
+        return Task.CompletedTask;
+    }
+
+    private void OnInputFileChanged(InputFileChangeEventArgs arg)
+    {
+        AuthorForCreateVm!.Photo = arg.File;
+    }
 }
